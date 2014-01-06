9c5c75deef43f06d3897a366425925f0952d9219
Bertie Wheen <wheen.b@gmail.com>
2014-01-06 23:29:59 +0000

    Initial commit

---
+++ hott-torus.js
@@ -0,0 +1,76 @@
+window.onload = function() {
+    var c = document.getElementById('canvas');
+    var ctx = c.getContext('2d');
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    c.width = w;
+    c.height = h;
+    var timer = null;
+    var a = 1;
+    var b = 1;
+    var r1 = 1;
+    var r2 = 2;
+    var k1 = 300;
+    var k2 = 5;
+    var imageLinks = [
+        'http://i.imgur.com/U25mAsV.png',
+        'http://i.imgur.com/NCmx3DC.png',
+        'http://i.imgur.com/zfwRDkB.png'
+    ];
+    var images = [];
+    var loadedImages = 0;
+    for(var i = 0; i < imageLinks.length; i++) {
+        var image = new Image();
+        image.onload = function() {
+            loadedImages++;
+        }
+        image.src = imageLinks[i];
+        images.push(image);
+    }
+
+    setInterval(function() {
+        ctx.fillStyle = '#abf';//'rgba(80, 90, 109)';
+        ctx.fillRect(0, 0, w, h);
+        a += 0.07;
+        b += 0.03;
+        var sina = Math.sin(a);
+        var sinb = Math.sin(b);
+        var cosa = Math.cos(a);
+        var cosb = Math.cos(b);
+        var twopi = 2 * Math.PI;
+        var i = 0;
+        for(var theta = 0; theta < twopi; theta += 0.628) {
+            var sintheta = Math.sin(theta);
+            var costheta = Math.cos(theta);
+            for(var phi = 0; phi < twopi; phi += 0.314) {
+                var sinphi = Math.sin(phi);
+                var cosphi = Math.cos(phi);
+                var objx = costheta*r1 + r2;
+                var objy = sintheta*r1;
+                var x = objx*(cosb*cosphi + sina*sinb*sinphi)
+                    - objy*cosa*sinb;
+                var y = objx*(sinb*cosphi - sina*cosb*sinphi)
+                    + objy*cosa*cosb;
+                var z = objx*cosa*sinphi + objy*sina + k2;
+                var sx = w/2 + x*k1*(1/z);
+                var sy = h/2 - y*k1*(1/z);
+                var lum = 0.7*(cosphi*costheta*sinb
+                    - cosa*costheta*sinphi
+                    - sina*sintheta
+                    + cosb*(cosa*sintheta - costheta*sina*sinphi));
+                /*if(lum > 0) {
+                    ctx.fillStyle = 'rgba(255,255,255,' + lum + ')';
+                    ctx.fillRect(sx, sy, 1.5, 1.5);
+                }*/
+                if(loadedImages < images.length) {
+                    ctx.fillStyle = '#fff';
+                    ctx.fillRect(sx, sy, 1.5, 1.5);
+                } else {
+                    var image = images[i % images.length];
+                    ctx.drawImage(image, sx - image.width/2, sy - image.height/2);
+                }
+                i++;
+            }
+        }
+    }, 50);
+}
